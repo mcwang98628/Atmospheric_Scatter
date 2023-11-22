8d4cb0d9c62607580184f992cb1459beeecfeac2
mcwang <mcwang98628@gmail.com>
2023-11-21 23:36:08 -0800

    feat: decouple descriptorsetlayout

--- main.cpp
+++ main.cpp
@@ -168,7 +168,8 @@ private:
 
         vulkanController->createRenderPass();
         renderPass = vulkanController->renderPass;
-        createDescriptorSetLayout();
+        vulkanController->createDescriptorSetLayout();
+        descriptorSetLayout = vulkanController->descriptorSetLayout;
         createGraphicsPipeline();
         createCommandPool();
         createDepthResources();
@@ -278,32 +279,6 @@ private:
         vulkanController->createImageViews();
         createDepthResources();
         createFramebuffers();
-    }
-
-    void createDescriptorSetLayout() {
-        VkDescriptorSetLayoutBinding uboLayoutBinding{};
-        uboLayoutBinding.binding = 0;
-        uboLayoutBinding.descriptorCount = 1;
-        uboLayoutBinding.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
-        uboLayoutBinding.pImmutableSamplers = nullptr;
-        uboLayoutBinding.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
-
-        VkDescriptorSetLayoutBinding samplerLayoutBinding{};
-        samplerLayoutBinding.binding = 1;
-        samplerLayoutBinding.descriptorCount = 1;
-        samplerLayoutBinding.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
-        samplerLayoutBinding.pImmutableSamplers = nullptr;
-        samplerLayoutBinding.stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT;
-
-        std::array<VkDescriptorSetLayoutBinding, 2> bindings = {uboLayoutBinding, samplerLayoutBinding};
-        VkDescriptorSetLayoutCreateInfo layoutInfo{};
-        layoutInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
-        layoutInfo.bindingCount = static_cast<uint32_t>(bindings.size());
-        layoutInfo.pBindings = bindings.data();
-
-        if (vkCreateDescriptorSetLayout(device, &layoutInfo, nullptr, &descriptorSetLayout) != VK_SUCCESS) {
-            throw std::runtime_error("failed to create descriptor set layout!");
-        }
     }
 
     void createGraphicsPipeline() {
